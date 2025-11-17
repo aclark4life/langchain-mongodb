f33fcc97a6a5a068d121fc0ae32c47a86101cf3b
Jeffrey A. Clark <aclark@aclark.net>
2025-11-17 07:11:25 -0500

    Integrate pymongo-search-utils
    - Remove vector search index funcs from langchain-mongodb and import from
      pymongo-search-utils.
    - Remove code from bulk_embed_and_insert_texts class method
      and replace with call to bulk_embed_and_insert_texts from
      pymongo-search-utils.
    - Replace create_fulltext_search_index
    - Replace combine_pipelines
    - Replace final_hybrid_stage
    - Update reciprocal_rank_stage

--- libs/langchain-mongodb/langchain_mongodb/pipelines.py
+++ libs/langchain-mongodb/langchain_mongodb/pipelines.py
@@ -9,6 +9,12 @@ See the following for more:
 
 from typing import Any, Dict, List, Optional, Union
 
+from pymongo_search_utils import (
+    combine_pipelines,  # noqa: F401
+    final_hybrid_stage,  # noqa: F401
+    reciprocal_rank_stage,  # noqa: F401
+)
+
 
 def text_search_stage(
     query: str,
@@ -89,74 +95,3 @@ def vector_search_stage(
     if filter:
         stage["filter"] = filter
     return {"$vectorSearch": stage}
-
-
-def combine_pipelines(
-    pipeline: List[Any], stage: List[Dict[str, Any]], collection_name: str
-) -> None:
-    """Combines two aggregations into a single result set in-place."""
-    if pipeline:
-        pipeline.append({"$unionWith": {"coll": collection_name, "pipeline": stage}})
-    else:
-        pipeline.extend(stage)
-
-
-def reciprocal_rank_stage(
-    score_field: str, penalty: float = 0, weight: float = 1, **kwargs: Any
-) -> List[Dict[str, Any]]:
-    """
-    Stage adds Weighted Reciprocal Rank Fusion (WRRF) scoring.
-
-    First, it groups documents into an array, assigns rank by array index,
-    and then computes a weighted RRF score.
-
-    Args:
-        score_field: A unique string to identify the search being ranked.
-        penalty: A non-negative float (e.g., 60 for RRF-60). Controls the denominator.
-        weight: A float multiplier for this source's importance.
-        **kwargs: Ignored; allows future extensions or passthrough args.
-
-    Returns:
-        Aggregation pipeline stage for weighted RRF scoring.
-    """
-
-    return [
-        {"$group": {"_id": None, "docs": {"$push": "$$ROOT"}}},
-        {"$unwind": {"path": "$docs", "includeArrayIndex": "rank"}},
-        {
-            "$addFields": {
-                f"docs.{score_field}": {
-                    "$multiply": [
-                        weight,
-                        {"$divide": [1.0, {"$add": ["$rank", penalty, 1]}]},
-                    ]
-                },
-                "docs.rank": "$rank",
-                "_id": "$docs._id",
-            }
-        },
-        {"$replaceRoot": {"newRoot": "$docs"}},
-    ]
-
-
-def final_hybrid_stage(
-    scores_fields: List[str], limit: int, **kwargs: Any
-) -> List[Dict[str, Any]]:
-    """Sum weighted scores, sort, and apply limit.
-
-    Args:
-        scores_fields: List of fields given to scores of vector and text searches
-        limit: Number of documents to return
-
-    Returns:
-        Final aggregation stages
-    """
-
-    return [
-        {"$group": {"_id": "$_id", "docs": {"$mergeObjects": "$$ROOT"}}},
-        {"$replaceRoot": {"newRoot": "$docs"}},
-        {"$set": {score: {"$ifNull": [f"${score}", 0]} for score in scores_fields}},
-        {"$addFields": {"score": {"$add": [f"${score}" for score in scores_fields]}}},
-        {"$sort": {"score": -1}},
-        {"$limit": limit},
-    ]
